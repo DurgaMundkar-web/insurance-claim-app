13f693cb6e87cae7dad75a74c6deac192d446d8b
kalki-2812 <kalkisureshbabu2812@gmail.com>
2026-02-17 19:22:58 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <div>
+      <h1>Internship Project</h1>
+      <Signup />
+      <Login />
+    </div>
+  );
+}
+
+export default App;

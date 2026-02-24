cfd37db38d4a08c0957f9f5ca331fdf211ecdf1b
Durga Mundkar <your-github-email@example.com>
2026-02-24 21:56:29 +0530

    Removed Marathi comment from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -84,7 +84,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* जेव्हा तुम्ही localhost:3000 उघडाल, तेव्हा थेट शिफारस (Recommendation) पेज दिसेल */}
+        
         <Route path="/" element={<Recommendation />} />
         <Route path="/recommendations" element={<Recommendation />} />
       </Routes>

cdb2bb8880e7bb51fc00984a410fe91b6af71bd7
Durga Mundkar <your-github-email@example.com>
2026-03-11 18:08:17 +0530

    Fixed all merge conflicts in package files

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Initialize the root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(

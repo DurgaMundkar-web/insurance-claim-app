29db30347b9e16ff79e90d03220252f4d0dbca91
Durga Mundkar <your-github-email@example.com>
2026-03-26 15:54:13 +0530

    Refactor: moved files to server folder, removed database.py and pyc files, and updated gitignore

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,24 @@
+import axios from 'axios';
 
-import axios from "axios";
-const API_BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:8000";
-const API = axios.create({ baseURL: API_BASE_URL });
-export default API;
+
+const API_URL = import.meta.env.VITE_API_BASE_URL; 
+
+export const submitClaimData = async (claimData) => {
+    try {
+        const response = await axios.post(`${API_URL}/submit-claim`, claimData);
+        return response.data;
+    } catch (error) {
+        console.error("Error submitting claim:", error);
+        throw error;
+    }
+};
+
+export const fetchAllClaims = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/get-claims`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching claims:", error);
+        throw error;
+    }
+};
